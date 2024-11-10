3be9f01defca9669e868b6197ec6ad09376faaa5
94359 <zenghao.chen@mail.utoronto.ca>
2024-11-10 14:10:25 -0500

    add empty p3/ changed from origin lab5

--- src/main/java/interface_adapter/change_password/LoggedInViewModel.java
+++ src/main/java/interface_adapter/change_password/LoggedInViewModel.java
@@ -6,6 +6,7 @@ import interface_adapter.ViewModel;
  * The View Model for the Logged In View.
  */
 public class LoggedInViewModel extends ViewModel<LoggedInState> {
+    public static final String SEARCH_LABEL = "Input what you want to search";
 
     public LoggedInViewModel() {
         super("logged in");
